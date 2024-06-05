2073ef15ae035b0829cf2e0206d968c737179c46
Paulo Queiroz <16908491+raggesilver@users.noreply.github.com>
2024-06-05 16:47:35 -0300

    Remove leftover log

--- server/routes/api/workspace/index.get.ts
+++ server/routes/api/workspace/index.get.ts
@@ -1,13 +1,7 @@
 import { getAllWorkspaces } from "~/server/services/workspace";
 
 export default defineEventHandler(async (event) => {
-  const session = await getUserSession(event);
-
-  if (!session.user) {
-    console.log("No user", JSON.stringify(event.node.req, null, 2));
-
-    throw createError({ status: 401, message: "Unauthorized" });
-  }
+  const session = await requireUserSession(event);
 
   return getAllWorkspaces(session.user.id);
 });

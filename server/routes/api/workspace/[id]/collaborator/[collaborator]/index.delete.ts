e457afc7c53578bbf172e7251d11c0958feff365
Paulo Queiroz <16908491+raggesilver@users.noreply.github.com>
2024-06-11 12:48:04 -0300

    Workspace collaborator end-points

---
+++ server/routes/api/workspace/[id]/collaborator/[collaborator]/index.delete.ts
@@ -0,0 +1,21 @@
+import { removeCollaboratorById } from "~/server/services/workspace";
+
+export default defineEventHandler(async (event) => {
+  await requireUserSession(event);
+
+  const id = getRouterParam(event, "id")!;
+  const collaboratorId = getRouterParam(event, "collaborator")!;
+
+  const result = await removeCollaboratorById(id, collaboratorId);
+
+  if (!result) {
+    // We're not sure exactly what went wrong here, so we'll just return a
+    // generic error
+    throw createError({
+      status: 400,
+      message: "Failed to remove collaborator from workspace",
+    });
+  }
+
+  return "OK";
+});

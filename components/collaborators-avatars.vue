03a6bc609c5d4d8c728db64ceec68e80efd9b738
Paulo Queiroz <16908491+raggesilver@users.noreply.github.com>
2024-06-14 13:44:19 -0300

    Render current user first in avatars' list

--- components/collaborators-avatars.vue
+++ components/collaborators-avatars.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import type { UnwrapRef } from "vue";
 import {
   Tooltip,
   TooltipContent,
@@ -10,11 +11,21 @@ const props = defineProps<{
   workspaceId: string;
 }>();
 
+const { user } = useUserSession();
+
 const { data: collaborators, suspense } = useWorkspaceCollaborators(
   props.workspaceId,
 );
 
 await suspense();
+
+const sortedCollaborators = computed(
+  () =>
+    // Move the current user to the front of the list. Everthing else is untouched.
+    collaborators.value?.toSorted((a, b) =>
+      a.id === user.value?.id ? -1 : b.id === user.value?.id ? 1 : 0,
+    ) ?? [],
+);
 
 const getFullNameInitials = (fullName: string) =>
   fullName
@@ -25,43 +36,46 @@ const getFullNameInitials = (fullName: string) =>
 </script>
 
 <template>
-  <div class="flex flex-row items-center wrapper">
-    <TooltipProvider
-      v-if="collaborators?.length"
-      v-for="(collaborator, i) of collaborators"
+  <ul v-if="collaborators?.length" class="flex flex-row items-center wrapper">
+    <li
+      v-for="collaborator of sortedCollaborators"
       :key="collaborator.id"
+      class="flex"
     >
-      <Tooltip>
-        <TooltipTrigger as-child>
-          <Avatar
-            class="avatar border border-foreground"
-            :class="i === 0 && 'z-10'"
-          >
-            <AvatarImage
-              v-if="collaborator.profilePictureUrl"
-              :src="collaborator.profilePictureUrl"
-            />
-            <AvatarFallback>{{
-              getFullNameInitials(collaborator.fullName)
-            }}</AvatarFallback>
-          </Avatar>
-        </TooltipTrigger>
-        <TooltipContent side="bottom">
-          <p>{{ collaborator.fullName }}</p>
-        </TooltipContent>
-      </Tooltip>
-    </TooltipProvider>
-  </div>
+      <TooltipProvider>
+        <Tooltip>
+          <TooltipTrigger as-child>
+            <Avatar class="avatar border border-foreground">
+              <AvatarImage
+                v-if="collaborator.profilePictureUrl"
+                :src="collaborator.profilePictureUrl"
+              />
+              <AvatarFallback>{{
+                getFullNameInitials(collaborator.fullName)
+              }}</AvatarFallback>
+            </Avatar>
+          </TooltipTrigger>
+          <TooltipContent side="bottom">
+            <p>{{ collaborator.fullName }}</p>
+          </TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
+    </li>
+  </ul>
   <!-- TODO: render round Skeletons during loading -->
 </template>
 
 <style scoped>
-.avatar:not(:first-child) {
+.wrapper li:first-child {
+  @apply z-10;
+}
+
+.wrapper li:not(:first-child) .avatar {
   @apply -ml-2 transition-all;
 }
 
-.wrapper:hover .avatar:not(:first-child),
-.wrapper:focus-within .avatar:not(:first-child) {
+.wrapper:hover li:not(:first-child) .avatar,
+.wrapper:focus-within li:not(:first-child) .avatar {
   @apply ml-1;
 }
 </style>

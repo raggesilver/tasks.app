c4a88944ed7546686cfbc638d120bede8ff3e70d
Paulo Queiroz <16908491+raggesilver@users.noreply.github.com>
2024-06-16 10:15:10 -0300

    Handle login redirects

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -2,6 +2,25 @@ export default defineNuxtRouteMiddleware((to) => {
   const { loggedIn } = useUserSession();
 
   if (!loggedIn.value && to.path.startsWith("/app")) {
-    return navigateTo({ path: "/", query: { redirectTo: to.fullPath } });
+    return navigateTo({ path: "/sign-in", query: { redirectTo: to.fullPath } });
+  }
+
+  const redirectTo = useCookie("signInRedirect", {
+    path: "/",
+    maxAge: 60 * 5,
+  });
+
+  // We store a redirect path in a cookie when the user is not logged in
+  // and tries to access a protected route. Here we check if the user is
+  // logged in and if there is a redirect path in the cookie. If both
+  // conditions are met, we redirect the user to the stored path.
+  if (
+    loggedIn.value &&
+    typeof redirectTo.value === "string" &&
+    to.path !== redirectTo.value
+  ) {
+    const path = redirectTo.value;
+    redirectTo.value = null;
+    return navigateTo({ path });
   }
 });

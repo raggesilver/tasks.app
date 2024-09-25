7054d1ce27855035df0f42c5a80a74d1452c03a8
Paulo Queiroz <16908491+raggesilver@users.noreply.github.com>
2024-09-25 15:21:03 -0300

    User API authorization

--- server/routes/api/user/[id]/index.get.ts
+++ server/routes/api/user/[id]/index.get.ts
@@ -1,16 +1,18 @@
+import { publicUserSchema, validateId } from "~/lib/validation";
 import { getUserById } from "~~/server/services/user";
 
 export default defineEventHandler(async (event) => {
   await requireUserSession(event);
-  const userId = getRouterParam(event, "id")!;
+  const { id } = await getValidatedRouterParams(
+    event,
+    validateId("id").parseAsync,
+  );
 
-  const user = await getUserById(userId);
+  const user = await getUserById(id);
 
   if (!user) {
     throw createError({ status: 404, message: "User not found" });
   }
 
-  const { fullName, id, profilePictureUrl } = user;
-
-  return { fullName, id, profilePictureUrl };
+  return publicUserSchema.parseAsync(user);
 });
--- server/routes/api/user/search.get.ts
+++ server/routes/api/user/search.get.ts
@@ -4,9 +4,15 @@ import { searchUsers } from "~~/server/services/user";
 export default defineEventHandler(async (event) => {
   await requireUserSession(event);
 
+  // TODO: we still need to set better rules for how users are able to search
+  // for others. Currently, any user can search for any other user, which is
+  // (likely) not ideal. In the future, we may want to narrow the search on a
+  // workspace basis — users will only be able to search for other users within
+  // the same workspace.
+  //
+  // The same goes for the get user endpoint.
+
   const data = await getValidatedQuery(event, searchUsersSchema.parseAsync);
 
-  const users = await searchUsers(data);
-
-  return publicUserSchema.array().parseAsync(users);
+  return searchUsers(data).then(publicUserSchema.array().parseAsync);
 });

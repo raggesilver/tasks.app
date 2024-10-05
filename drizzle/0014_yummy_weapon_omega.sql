bca2b2e56d1c4e9baceac8e42845d62c0615253d
Paulo Queiroz <16908491+raggesilver@users.noreply.github.com>
2024-10-05 08:16:28 -0300

    Drop outdated migrations

--- drizzle/0014_yummy_weapon_omega.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE "attachments" RENAME COLUMN "original_name" TO "name";--> statement-breakpoint
-ALTER TABLE "attachments" ADD COLUMN "mime_type" varchar(255) NOT NULL;--> statement-breakpoint
-ALTER TABLE "attachments" ADD COLUMN "size" integer NOT NULL;

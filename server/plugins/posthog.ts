0b513e105ff6ca1de4136f231f12877f316985e1
Paulo Queiroz <16908491+raggesilver@users.noreply.github.com>
2024-12-27 00:43:06 -0300

    Disable Posthog plugin during prerender

--- server/plugins/posthog.ts
+++ server/plugins/posthog.ts
@@ -1,6 +1,8 @@
 import consola from "consola";
 
 export default defineNitroPlugin((nitro) => {
+  if (import.meta.prerender) return;
+
   consola.success("Posthog plugin loaded 🦔");
 
   nitro.hooks.hookOnce("close", async () => {
